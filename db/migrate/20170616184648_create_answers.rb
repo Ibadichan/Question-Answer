0457807fb334d85038df84f3c008b83f85147e1e
Ivan Badichan <badican01117@gmail.com>
2017-06-16 21:13:37 +0200

    lesson-24

---
+++ db/migrate/20170616184648_create_answers.rb
@@ -0,0 +1,8 @@
+class CreateAnswers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :answers do |t|
+      t.text :body
+      t.belongs_to :question, index: true
+    end
+  end
+end
