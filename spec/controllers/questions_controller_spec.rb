711ff2bbae4b7270c0f2a876e5bd2f9bf09ba1b1
Ivan Badichan <badican01117@gmail.com>
2017-06-20 11:38:16 +0200

    lesson 25

---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe QuestionsController do
+  let(:question) { create(:question) }
+
+  describe 'GET #show' do
+    before { get :show, params: { id: question } }
+
+    it 'assigns the requested Question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'assigns a new Question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves the new question in the database' do
+        expect { post :create, params: { question: attributes_for(:question) } }.to change { Question.count }.by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save question in database' do
+        expect { post :create, params: { question: attributes_for(:invalid_question) } }.to_not change(Question.count)
+      end
+
+      it 'renders new view' do
+        post :create, params: { question: attributes_for(:invalid_question) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+end
---
+++ spec/factories/answers.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+FactoryGirl.define do
+  factory :answer do
+    body  'MyText'
+    question
+  end
+
+  factory :invalid_answer, class: 'Answer' do
+    body  nil
+    question
+  end
+end
