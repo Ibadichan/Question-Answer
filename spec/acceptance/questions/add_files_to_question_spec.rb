43130306f926de21c21f805f0a64cb8a72c15b9f
Ivan Badichan <badican01117@gmail.com>
2017-07-19 23:24:07 +0200

    Add one file to question,lesson-29

--- spec/acceptance/questions/add_files_to_question_spec.rb
+++ spec/acceptance/questions/add_files_to_question_spec.rb
@@ -13,11 +13,13 @@ feature 'User can add files to question', '
   scenario 'User tries to add files to question' do
     sign_in user
     visit new_question_path
+
     fill_in 'Заголовок', with: 'title'
     fill_in 'Ваш вопрос', with: 'body'
     attach_file 'Файл', "#{Rails.root}/spec/spec_helper.rb"
     click_on 'Создать'
 
-    expect(page).to have_content 'spec_helper.rb'
+    expect(page).to have_link 'spec_helper.rb',
+                              href: '/uploads/attachment/file/1/spec_helper.rb'
   end
 end
