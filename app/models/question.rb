b7dd6190a6c01568869e59ced1a013d72cb662d3
Ivan Badichan <badican01117@gmail.com>
2017-07-21 20:34:54 +0200

    Revert "Question accepts nested attributes for attachments, lesson-29"
    This reverts commit 29183dae0a90558ad3a1a3318bdd05140b6ac655.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 class Question < ApplicationRecord
-  belongs_to :user
   has_many :answers, dependent: :destroy
   has_many :attachments, as: :attachable, dependent: :destroy
+  belongs_to :user
 
   validates :title, :body, presence: true
-
-  accepts_nested_attributes_for :attachments
 end
