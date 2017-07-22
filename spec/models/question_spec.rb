7b062a8a227f448bfe9eca0022d5f5b2d8105abb
Ivan Badichan <badican01117@gmail.com>
2017-07-22 21:56:42 +0200

    Attachable concern model spec, lesson-30

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require_relative 'concerns/attachable'
 
 RSpec.describe Question, type: :model do
   it { should belong_to(:user) }
   it { should have_many(:answers).dependent(:destroy) }
-  it { should have_many(:attachments).dependent(:destroy) }
+  it_behaves_like 'attachable'
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
-
-  it { should accept_nested_attributes_for :attachments }
 end
