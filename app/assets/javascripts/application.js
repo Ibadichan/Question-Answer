d306081a5c38c2ddd0b6ee20d15b2b4e49ea27d4
Ivan Badichan <badican01117@gmail.com>
2017-07-31 11:40:28 +0200

    Refactoring 2,lesson-30

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,7 @@
 //= require jquery_ujs
 //= require jquery.remotipart
 //= require turbolinks
+//= require cocoon
 //= require twitter/bootstrap
 //= require_tree ./templates
 //= require_tree .
-//= require cocoon
