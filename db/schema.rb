b96cf6951e17ec7b692e328732b25352e437f653
Ivan Badichan <badican01117@gmail.com>
2017-08-06 13:58:10 +0200

    Stream questions, lesson-31

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,64 +10,65 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_170_725_164_105) do
+ActiveRecord::Schema.define(version: 20170725164105) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'answers', force: :cascade do |t|
-    t.text 'body'
-    t.bigint 'question_id'
-    t.bigint 'user_id'
-    t.boolean 'best', default: false
-    t.index ['question_id'], name: 'index_answers_on_question_id'
-    t.index ['user_id'], name: 'index_answers_on_user_id'
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.bigint "question_id"
+    t.bigint "user_id"
+    t.boolean "best", default: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
-  create_table 'attachments', force: :cascade do |t|
-    t.string 'file'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'attachable_id'
-    t.string 'attachable_type'
-    t.index %w[attachable_id attachable_type], name: 'index_attachments_on_attachable_id_and_attachable_type'
+  create_table "attachments", force: :cascade do |t|
+    t.string "file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "attachable_id"
+    t.string "attachable_type"
+    t.index ["attachable_id", "attachable_type"], name: "index_attachments_on_attachable_id_and_attachable_type"
   end
 
-  create_table 'questions', force: :cascade do |t|
-    t.string 'title'
-    t.text 'body'
-    t.bigint 'user_id'
-    t.index ['user_id'], name: 'index_questions_on_user_id'
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer 'sign_in_count', default: 0, null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.inet 'current_sign_in_ip'
-    t.inet 'last_sign_in_ip'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'name'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table 'votes', force: :cascade do |t|
-    t.integer 'value'
-    t.bigint 'user_id'
-    t.string 'votable_type'
-    t.bigint 'votable_id'
-    t.index ['user_id'], name: 'index_votes_on_user_id'
-    t.index %w[votable_type votable_id], name: 'index_votes_on_votable_type_and_votable_id'
+  create_table "votes", force: :cascade do |t|
+    t.integer "value"
+    t.bigint "user_id"
+    t.string "votable_type"
+    t.bigint "votable_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+    t.index ["votable_type", "votable_id"], name: "index_votes_on_votable_type_and_votable_id"
   end
 
-  add_foreign_key 'answers', 'questions'
-  add_foreign_key 'answers', 'users'
-  add_foreign_key 'questions', 'users'
-  add_foreign_key 'votes', 'users'
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
+  add_foreign_key "questions", "users"
+  add_foreign_key "votes", "users"
 end
