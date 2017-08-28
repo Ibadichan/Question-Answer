b8d1e63a4d76e457051bc6126488fe7163bdb575
Ivan Badichan <badican01117@gmail.com>
2017-08-28 21:07:59 +0200

    All profiles API, lesson-35

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -4,4 +4,8 @@ class Api::V1::ProfilesController < Api::V1::BaseController
   def me
     respond_with current_resource_owner
   end
+
+  def all
+    respond_with User.where.not(id: current_resource_owner.id)
+  end
 end
