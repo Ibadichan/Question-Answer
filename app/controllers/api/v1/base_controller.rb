fe9114d78b7175582f1f78e27c81edb0a5c3813d
Ivan Badichan <badican01117@gmail.com>
2017-08-31 22:20:58 +0200

    Create Answer API, lesson-36

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -5,10 +5,6 @@ class Api::V1::BaseController < ActionController::Base
   respond_to :json
   check_authorization
 
-  rescue_from CanCan::AccessDenied do |exception|
-    render json: { error: exception.message }, status: :forbidden
-  end
-
   protected
 
   def current_resource_owner
