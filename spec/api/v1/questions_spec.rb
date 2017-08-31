de42976d9b421dda7d499b7ea5b962d828fdfcd3
Ivan Badichan <badican01117@gmail.com>
2017-08-31 17:41:36 +0200

    Create Question API, lesson-36

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -133,4 +133,68 @@ describe 'Questions API' do
       end
     end
   end
+
+  describe 'POST #create' do
+    context 'Unauthorized' do
+      it 'returns 401 status if access token was not submitted' do
+        post '/api/v1/questions', params: { question: attributes_for(:question),
+                                            format: :json }
+        expect(response.status).to eq 401
+      end
+
+      it 'returns 401 status if access token is invalid' do
+        post '/api/v1/questions', params: { question: attributes_for(:question),
+                                            access_token: '123', format: :json }
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'Authorized' do
+      let(:access_token) { create(:access_token) }
+
+      it 'returns status 200' do
+        post '/api/v1/questions', params: { access_token: access_token.token,
+                                            question: attributes_for(:question),
+                                            format: :json }
+        expect(response).to be_success
+      end
+
+      it 'creates a new question' do
+        expect do
+          post '/api/v1/questions', params: { access_token: access_token.token,
+                                              question: attributes_for(:question),
+                                              format: :json }
+        end.to change(Question, :count).by(1)
+      end
+
+      it 'returns the question' do
+        post '/api/v1/questions', params: { access_token: access_token.token,
+                                            question: attributes_for(:question),
+                                            format: :json }
+        expect(response.body).to have_json_size(1)
+      end
+
+      %w[id title body created_at updated_at user_id].each do |attr|
+        it "contains #{attr} of question" do
+          post '/api/v1/questions', params: { access_token: access_token.token,
+                                              question: attributes_for(:question),
+                                              format: :json }
+
+          expect(response.body).to be_json_eql(
+            assigns(:question).send(attr.to_sym).to_json
+          ).at_path("question/#{attr}")
+        end
+      end
+
+      it 'contains short title of question' do
+        post '/api/v1/questions', params: { access_token: access_token.token,
+                                            question: attributes_for(:question),
+                                            format: :json }
+
+        expect(response.body).to be_json_eql(
+          assigns(:question).title.truncate(10).to_json
+        ).at_path('question/short_title')
+      end
+    end
+  end
 end
