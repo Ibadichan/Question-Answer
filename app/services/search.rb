b2800a7d8f55d66c7905d6e5f91dcb859ad1760c
Ivan Badichan <badican01117@gmail.com>
2017-09-15 10:12:45 +0200

    Improvements, lesson-39

--- app/services/search.rb
+++ app/services/search.rb
@@ -1,20 +1,14 @@
 # frozen_string_literal: true
 
 class Search
-  CATEGORIES = ['Вопросы', 'Ответы', 'Комментарии', 'Пользователи', 'Все категории'].freeze
+  CATEGORIES         = ['Вопросы', 'Ответы', 'Комментарии', 'Пользователи', 'Все категории'].freeze
+  ENGLISH_CATEGORIES = ['Questions', 'Answers', 'Comments', 'Users', 'All Categories'].freeze
 
   def self.find_object(query, category)
-    case category
-    when 'Вопросы'
-      Question.search(query)
-    when 'Ответы'
-      Answer.search query
-    when 'Комментарии'
-      Comment.search query
-    when 'Пользователи'
-      User.search query
-    when 'Все категории'
-      ThinkingSphinx.search query
-    end
+    query = Riddle::Query.escape(query)
+    return ThinkingSphinx.search query if category == 'Все категории'
+    index = CATEGORIES.index(category)
+    english_category = ENGLISH_CATEGORIES[index]
+    english_category.classify.constantize.search(query)
   end
 end
